b71cb01f241b63c40a4097dfc794b5d311519a29
dudux7 <eduardogerebatista556@gmail.com>
2024-09-10 17:06:15 -0300

    upando função de informar nome e função de informar idade

--- list1_atv1.php
+++ list1_atv1.php
@@ -1,63 +1,88 @@
 <?php
 //testando classes e obejetos
     class Pessoa {
-        private $idade;
-        public function calculaIdade ($idade) {
-            
+        public $diaAtual;
+        public $diaNasc;
+
+        public $mesAtual;
+        public $mesNasc;
+
+        public $anoAtual;
+        public $anoNasc;
 
 //usuario digite a data atual
-            echo "digite o dia atual";
-            $diaAtual = readline();
+        public function DataAtual (){
+            echo "digite o dia atual: \n";
+            $this->diaAtual = readline();
         
-            echo "digite o mês atual";
-            $mesAtual = readline ();
+            echo "digite o mês atual: \n";
+            $this->mesAtual = readline ();
         
-            echo "digite o ano atual";
-            $anoAtual = readline();
-        
+            echo "digite o ano atual: \n";
+            $this->anoAtual = readline();
+        }
+
+//usuario digite a data atual
+        public function DataNasc () {
             echo "digite o dia do seu nascimento: \n";
-            $diaNasc = readline();
+            $this->diaNasc = readline();
         
             echo "digite o mes do seu nascimento: \n";
-            $mesNasc = readline();
+            $this->mesNasc = readline();
         
             echo "digite o ano do seu nascimento: \n";
-            $anoNasc = readline();
-            
-            $idade = $anoAtual - $anoNasc;
-
-                if ($mesAtual < $mesNasc || ($mesAtual == $mesNasc && $diaAtual < $diaNasc)) {
-                    $idade--;
-                }
+            $this->anoNasc = readline();
             
         }
 
-        
+        public $idade;
+// função calcular idade
+        public function calculaIdade () {
+            $this->DataAtual ();
+            $this->DataNasc ();
+            $this->idade = $this->anoAtual - $this->anoNasc;
+
+                if ($this->mesAtual < $this->mesNasc || ($this->mesAtual == $this->mesNasc && $this->diaAtual < $this->diaNasc)) {
+                    $this->idade--;
+                }
+        }
+
 // função para chamar a idade
         public function informaIdade(){
-
+            echo "a sua idade é:  " . $this->idade . "\n";
         }
-            
-        
 
+//nomes
+        public $nomeDudu = "Dudu";
+        public $nomeJoao = "Joao";
+        public $n;
         
-        //nomes
-        public $nomeDudu = "Eduardo ";
-        public $nomeJoao = "Joao ";
-        
+        public function NomeEscolhido($n) {
+            $this->n = $n;
+        }
         public function informaNome() {
-            
-            
-            
+            if ($this->n === 1) {
+                return $this->nomeDudu . "\n";
+            } elseif ($this->n === 2) {
+                return $this->nomeJoao . "\n";
+            } else {
+                return "Escolha inválida.\n";
+            }
+              //  if ($this->n === 1) {
+                //    echo "seu nome é: " . $this->nomeDudu . "\n";
+                //}elseif ($this->n === 2) {
+                 //   echo "seu nome é: " . $this->nomeJoao . "\n";
+               // }
         }
     }
         
     $Dudu = new Pessoa;
-    $Dudu -> calculaIdade();
-    $Dudu -> informaIdade();
+    $Dudu->NomeEscolhido(1);
+        echo "bem vindo " . $Dudu->informaNome() . "\n";
+    $Dudu->calculaIdade();
 
-
-
+    $Dudu->informaIdade();
+        
 
     $Joao = new Pessoa;
 
